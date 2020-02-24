d5fe13e20e4fe529fb0091d70b9e9b45228a5637
Tarbadev <tarbadev@gmail.com>
2020-02-23 19:17:14 -0800

    Display turn number

--- lib/game/atoupic_game.dart
+++ lib/game/atoupic_game.dart
@@ -3,16 +3,29 @@ import 'dart:ui';
 import 'package:atoupic/application/domain/entity/player.dart';
 import 'package:atoupic/game/components/background.dart';
 import 'package:atoupic/game/components/player_component.dart';
+import 'package:flame/anchor.dart';
+import 'package:flame/components/text_component.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
+import 'package:flame/text_config.dart';
 
 class AtoupicGame extends BaseGame {
   Background _background;
   bool visible = false;
+  int _turn = 1;
+  TextComponent _turnComponent;
 
   AtoupicGame() {
     _background = Background(0xFF079992);
 
+    TextConfig regular = TextConfig(color: BasicPalette.white.color);
+    _turnComponent = TextComponent('Turn $_turn', config: regular)
+      ..x = 10
+      ..y = 5
+      ..anchor = Anchor.topLeft;
+
     add(_background);
+    add(_turnComponent);
   }
 
   @override
