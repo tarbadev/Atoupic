c8c99028011e955cde1c4ef0feebff7e304a9402
Tarbadev <tarbadev@gmail.com>
2020-03-14 10:00:08 -0700

    Fix design of next turn dialog

--- lib/application/ui/view/in_game_view.dart
+++ lib/application/ui/view/in_game_view.dart
@@ -126,59 +126,58 @@ class InGameView extends StatelessWidget {
       BuildContext context, TurnResultDisplay turnResultDisplay, Function onNextPressed) async {
     SchedulerBinding.instance.addPostFrameCallback(
       (_) => showDialog(
+        barrierDismissible: false,
         context: context,
-        child: SimpleDialog(
+        child: AlertDialog(
           key: Key('TurnResultDialog'),
-          title: Center(
-            child: Text(
-              turnResultDisplay.result,
-              key: Key('TurnResultDialog__Result'),
-              style: TextStyle(fontSize: 22.0),
-            ),
+          title: Text(
+            turnResultDisplay.result,
+            key: Key('TurnResultDialog__Result'),
+            style: TextStyle(fontSize: 22.0),
           ),
-          children: <Widget>[
-            Center(
-              child: Column(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Taker: ${turnResultDisplay.taker}',
+                key: Key('TurnResultDialog__Taker'),
+                style: TextStyle(fontSize: 22.0),
+              ),
+              Divider(
+                color: Colors.transparent,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Taker: ${turnResultDisplay.taker}',
-                    key: Key('TurnResultDialog__Taker'),
-                    style: TextStyle(fontSize: 22.0),
+                    turnResultDisplay.takerScore.toString(),
+                    key: Key('TurnResultDialog__TakerScore'),
+                    style: TextStyle(fontSize: 20.0),
                   ),
-                  Divider(
-                    color: Colors.transparent,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        turnResultDisplay.takerScore.toString(),
-                        key: Key('TurnResultDialog__TakerScore'),
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                      Container(
-                          height: 20,
-                          child: VerticalDivider(
-                            color: Colors.grey,
-                            thickness: 2,
-                          )),
-                      Text(
-                        turnResultDisplay.opponentScore.toString(),
-                        key: Key('TurnResultDialog__OpponentScore'),
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ],
-                  ),
-                  SimpleDialogOption(
-                    key: Key('TurnResultDialog__NextButton'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                      onNextPressed();
-                    },
-                    child: Text('Next'),
+                  Container(
+                      height: 20,
+                      child: VerticalDivider(
+                        color: Colors.grey,
+                        thickness: 2,
+                      )),
+                  Text(
+                    turnResultDisplay.opponentScore.toString(),
+                    key: Key('TurnResultDialog__OpponentScore'),
+                    style: TextStyle(fontSize: 20.0),
                   ),
                 ],
               ),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              key: Key('TurnResultDialog__NextButton'),
+              onPressed: () {
+                Navigator.pop(context);
+                onNextPressed();
+              },
+              child: Text('Next'),
             ),
           ],
         ),
